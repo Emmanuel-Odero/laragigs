40dcb4254fd0a9dbe34aa6bf7a6b80246e909333
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-06-19 15:37:37 +0300

    playing with blade basic syntax and feature

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,17 +1,20 @@
-<h1>{{$heading}}</h1>
-    @unless(count($listings) == 0)
-        
-        @foreach($listings as $listing)
-            <h2>
-                <a href="/listings/{{$listing['id']}}">
-                    {{$listing['title']}}
-                </a>
-            </h2>
-            <p>
-                {{$listing['description']}}
-            </p>
-        @endforeach
+@extends('layout')
+@section('content')
+        <h1>{{$heading}}</h1>
+        @unless(count($listings) == 0)
+            
+            @foreach($listings as $listing)
+                <h2>
+                    <a href="/listings/{{$listing['id']}}">
+                        {{$listing['title']}}
+                    </a>
+                </h2>
+                <p>
+                    {{$listing['description']}}
+                </p>
+            @endforeach
 
-        @else
-            <p>No Listings Found</p>
-    @endunless
+            @else
+                <p>No Listings Found</p>
+        @endunless
+@endsection
