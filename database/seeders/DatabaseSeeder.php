9a3bf6a67c492b815293bcb5b6261aadfa198912
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-06-19 15:20:43 +0300

    working with database seed and faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,7 +15,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+        Listing::factory(7)->create();
+        // Listing::create([
+        //         'title'=>'Laravel Senior Developer',
+        //         'tags'=>'Javascript, laravel, php',
+        //         'company'=>'Arora Ltd',
+        //         'location'=>'Nairobi, Kenya',
+        //         'email'=>'mail@arora.com',
+        //         'website'=>'www.arora.com',
+        //         'description'=>'
+        //         In publishing and graphic design, Lorem ipsum is a placeholder 
+        //         text commonly used to demonstrate the visual form of a document 
+        //         or a typeface without relying on meaningful content. Lorem ipsum 
+        //         may be used as a placeholder before final copy is available.
+        //         '
+        //     ]);
+        //     Listing::create([
+        //         'title'=>'Full-stack Engineer',
+        //         'tags'=>'Node, laravel, flask',
+        //         'company'=>'Jumbo Ltd',
+        //         'location'=>'Kampala, Uganda',
+        //         'email'=>'info@jumbo.ug',
+        //         'website'=>'www.jumbo.ug',
+        //         'description'=>'
+        //         In publishing and graphic design, Lorem ipsum is a placeholder 
+        //         text commonly used to demonstrate the visual form of a document 
+        //         or a typeface without relying on meaningful content. Lorem ipsum 
+        //         may be used as a placeholder before final copy is available.
+        //         '
+        //     ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
