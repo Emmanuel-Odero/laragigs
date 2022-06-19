e3f6a2bcfe75f265e06b1fec67cb7c4006cc056b
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-06-19 17:17:11 +0300

    now displays 404 error incase of wrong routing passed to the url

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,8 @@ Route::get('listings', function(){
         ]
     );
 });
-Route::get('/listings/{id}', function($id){
+Route::get('/listings/{listing}', function(Listing $listing){
     return view('listing', [
-        'listing'=>Listing::find($id)
+        'listing'=>$listing
     ]);
 });
