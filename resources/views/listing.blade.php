7e4d0d32d7214192b6268b52b2ac17481de1ecfd
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-06-22 01:00:54 +0300

    single listing

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -6,40 +6,19 @@
     ><i class="fa-solid fa-arrow-left"></i> Back
     </a>
     <div class="mx-4">
-        <div class="bg-gray-50 border border-gray-200 p-10 rounded">
-            <div
-                class="flex flex-col items-center justify-center text-center"
-            >
+        <x-card class="p-10">
+            <div class="flex flex-col items-center justify-center text-center">
                 <img
-                    class="w-48 mr-6 mb-6"
+                    class="w-10 mr-6 mb-6"
                     src="{{asset('images/no-image.png')}}"
                     alt=""
                 />
 
                 <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
                 <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
-                <ul class="flex">
-                    <li
-                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                    >
-                        <a href="#">Laravel</a>
-                    </li>
-                    <li
-                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                    >
-                        <a href="#">API</a>
-                    </li>
-                    <li
-                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                    >
-                        <a href="#">Backend</a>
-                    </li>
-                    <li
-                        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                    >
-                        <a href="#">Vue</a>
-                    </li>
-                </ul>
+                <div>
+                    <x-listing-tags :tagsCSV="$listing->tags"/>
+                </div>
                 <div class="text-lg my-4">
                     <i class="fa-solid fa-location-dot"></i>{{$listing->location}}
                 </div>
@@ -68,6 +47,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    </x-card>
 
 @endsection
