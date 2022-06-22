d2e24d25b38a4445957d6940376892e56a2dc2be
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-06-22 20:31:58 +0300

    comments showing all routes I will need

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,14 @@ Route::get('/index.html', function () {
 });
 Route::get('',[ListingController::class,'index']);
 Route::get('/listings/{listing}',[ListingController::class,'show']);
+
+// Common Routes:
+/*
+index ---shows all job listings
+show ---show all single listing
+create ---show form to create new liting
+store ---store new listing
+edit ---show form to edit listing
+update ---update listing
+destroy ---Delete listing
+*/
