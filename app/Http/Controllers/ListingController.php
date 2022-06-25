5a6685c40785baaca28c82bb4bece22cbbb39439
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-06-25 09:56:46 +0300

    Created listings now succefully submit to the datase

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -39,6 +39,7 @@ class ListingController extends Controller
             'tags'=>'required',
             'description'=>'required'
         ]);
+        Listing::create($formFields);
         return redirect('/');
     }
 }
